ed02a2a94924d7e39f1e057efe471dd6bd43027d
PriyeshRai369 <priyeshrai369@gmail.com>
2024-03-09 13:04:51 +0530

    new question

---
+++ checkBoxPractice/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [checked, setChecked] = useState({});
+  const data = ["play cricket", "watch movies", "code"];
+  const [newData, setNewData] = useState(data);
+
+  function HandleChange(e, index) {
+    const isChecked = e.target.checked;
+    setChecked(prevChecked => ({
+      ...prevChecked,
+      [index]: isChecked
+    }));
+  }
+
+  function HandleClick(index) {
+    const newVal = newData.filter((_, i) => i !== index);
+    setNewData(newVal);
+    // Remove checked status for the deleted item
+    setChecked(prevChecked => {
+      const updatedChecked = { ...prevChecked };
+      delete updatedChecked[index];
+      return updatedChecked;
+    });
+  }
+
+  return (
+    <ul>
+      {newData.map((value, index) => {
+        return (
+          <li key={index}>
+            <input 
+              type="checkbox" 
+              onChange={(e) => HandleChange(e, index)} 
+              checked={checked[index] || false} 
+            />
+            {value}
+            {checked[index] && <button onClick={() => HandleClick(index)}>Delete</button>}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default App;
+      
